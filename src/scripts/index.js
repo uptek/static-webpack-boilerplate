66f796cca5940e17c9eb656f1921e87f0d853064
Muhammad Hasnain <mhasnaindev@gmail.com>
2022-02-20 07:44:36 +0500

    update and refactor babel and eslint packages and disable rules caused due to bugs in packages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
+/* eslint-disable import/no-import-module-exports */
+
+/**
+ * To know why this rule was disabled...
+ *
+ * @see https://github.com/airbnb/javascript/issues/2496#issuecomment-1046147167
+ */
+
 if (module && module.hot) module.hot.accept();
